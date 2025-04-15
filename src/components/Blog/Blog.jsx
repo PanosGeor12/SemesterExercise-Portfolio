c702afaf02fa2a155ddac34f718f1aa5384b3d23
PanosG <panagiotisgeorgaris12@gmail.com>
2025-04-15 20:14:09 +0300

    added components and functionality

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,8 +1,10 @@
 import './Blog.css';
 import { useState, useEffect } from 'react'
-import parse from 'html-react-parser'
 import Title from '../Title';
 import axios from 'axios';
+import PostCard from './PostCard/PostCard';
+import BlogLayout from './BlogLayout/BlogLayout';
+import MainContent from '../MainContent/MainContent';
 
 export default function Blog() {
     const [posts, setPosts] = useState([]);
@@ -34,6 +36,7 @@ export default function Blog() {
                                     }
                                 }
                                 title
+                                slug
                                 content
                               }
                             }
@@ -54,36 +57,22 @@ export default function Blog() {
     return (
         <>
             <Title title="Blog" />
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto max-w-2xl lg:mx-0">
-                    <h2 className="text-4xl font-semibold tracking-tight text-pretty sm:text-5xl">Blog</h2>
-                    <p className="mt-2 text-lg/8">Read news from me and also learn how to build great things</p>
-                </div>
-                <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <MainContent>
+                <BlogLayout>
                     {(posts.length > 0)
                         ? posts.map((post) => (
-                            <article className="flex max-w-xl flex-col items-start justify-between" key={post.id}>
-                                <div className="flex items-center gap-x-4 text-xs">
-                                    <time dateTime={new Date(post.date).toDateString()} className="">{new Date(post.date).toDateString()}</time>
-                                    <a href="#" className="relative z-10 rounded-full bg-gray-50 text-black px-3 py-1.5 font-medium hover:bg-gray-100">{post.categories.nodes[0]?.name}</a>
-                                </div>
-                                <div className="group relative">
-                                    <h3 className="mt-3 text-lg/6 font-semibold group-hover">
-                                        <a href="#">
-                                            <span className="absolute inset-0"></span>
-                                            {post.title}
-                                        </a>
-                                    </h3>
-                                    <div className="mt-5 line-clamp-3 text-sm/6">{parse(post.content)}</div>
-                                </div>
-                                <div className="text-sm/6 py-5">
-                                    <p className="text-gray-600">{post.author.node.name}</p>
-                                </div>
-                            </article>
+                            <PostCard
+                                key={post.id}
+                                date={new Date(post.date).toDateString()}
+                                category={post.categories.nodes[0]?.name}
+                                title={post.title}
+                                content={post.content}
+                                author={post.author.node.name}
+                                slug={post.slug} />
                         ))
                         : <p>No posts</p>}
-                </div>
-            </div>
+                </BlogLayout>
+            </MainContent>
         </>
     )
 }
--- src/components/Blog/Post/Post.jsx
+++ src/components/Blog/Post/Post.jsx
@@ -1,13 +1,64 @@
 import './Post.css'
-import { useParams, useMatch } from 'react-router';
+import axios from 'axios';
+import { useMatch, useNavigate } from 'react-router';
+import { useState, useEffect } from 'react';
+import parse from 'html-react-parser';
+import MainContent from '../../MainContent/MainContent';
+import Title from '../../Title';
 
 export default function Post() {
+
     const match = useMatch('/blog/:slug');
     const postSlug = match.params["slug"];
+    const [post, setPost] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
 
+    useEffect(() => {
+
+        const fetchPost = async () => {
+            try {
+                const response = await axios({
+                    url: "https://wordpress.test/graphql",
+                    method: "post",
+                    data: {
+                        query: `query Post {
+                                post (id : "${postSlug}", idType : SLUG) {
+                                    title
+                                    content
+                                }
+                            }`
+                    }
+                });
+
+                if (!response.data.data.post) {
+                    setError(true)
+                } else {
+                    setPost(response.data.data.post)
+                    setLoading(false)
+                }
+            } catch (err) {
+
+            }
+        }
+        fetchPost();
+    }, [postSlug]);
+
+    useEffect(() => {
+        if (error) {
+            navigate("/blog");       
+        }
+    },[error])
     return (
         <>
-        
+            <Title title={post.title} />
+            <MainContent>
+                <div className='post'>
+                    <h2>{post.title}</h2>
+                    <div>{parse(String(post.content))}</div>
+                </div>
+            </MainContent>
         </>
     );
 }
